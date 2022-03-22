5ac9f15dd88beba0b9052f90c572e3825576b846
kwonjuyoung <20161527@vision.hoseo.edu>
2022-03-22 21:54:41 +0900

    붓 크기 조절

--- app/src/main/java/com/example/kotlindrawingapp/DrawingView.kt
+++ app/src/main/java/com/example/kotlindrawingapp/DrawingView.kt
@@ -3,6 +3,7 @@ package com.example.kotlindrawingapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -30,7 +31,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs){
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
+        //mBrushSize = 20.toFloat()
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -86,9 +87,17 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs){
             else -> return false
         }
         invalidate()
-
         return true
     }
+
+    fun setSizeForBrush(newSize : Float){
+    mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, newSize, resources.displayMetrics)
+        mDrawPaint!!.strokeWidth = mBrushSize
+
+    }
+
+
+
 
     internal inner class CustomPath(var color:Int, var brushThickness: Float): Path(){
 
