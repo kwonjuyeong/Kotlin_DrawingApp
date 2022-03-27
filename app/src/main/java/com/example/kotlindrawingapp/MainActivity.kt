11c8fc41c993e8173bbcf8e2a7e7878a45a503b9
kwonjuyoung <20161527@vision.hoseo.edu>
2022-03-28 01:47:47 +0900

    그린 파일 Bitmap으로 저장, Undo 기능

--- app/src/main/java/com/example/kotlindrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlindrawingapp/MainActivity.kt
@@ -3,15 +3,17 @@ package com.example.kotlindrawingapp
 
 import android.Manifest
 import android.app.Dialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.Color
 import android.media.Image
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.LinearLayout
-import android.widget.Toast
+import android.widget.*
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
@@ -19,6 +21,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,23 +36,29 @@ class MainActivity : AppCompatActivity() {
     private var drawingView: DrawingView ?= null
     private var mImageButtonCurrentPaint: ImageButton ?= null
 
+    val openGalleryLauncher:ActivityResultLauncher<Intent> = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result ->
+        if(result.resultCode == RESULT_OK && result.data!=null){
+            val imageBackgroud : ImageView = findViewById(R.id.pic_Background)
+            imageBackgroud.setImageURI(result.data?.data)
+        }
+    }
+
     val requestPermission: ActivityResultLauncher<Array<String>> =
         registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
             permissions ->
             permissions.entries.forEach{
                 val permissionName = it.key
                 val isGranted = it.value
-
                 if(isGranted){
                     Toast.makeText(this@MainActivity, "Permission granted now you can read the storage files.", Toast.LENGTH_LONG).show()
+                    val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    openGalleryLauncher.launch(pickIntent)
                 }else{
                     if(permissionName==Manifest.permission.READ_EXTERNAL_STORAGE){
                         Toast.makeText(this@MainActivity, "Oops you just denied the permissions", Toast.LENGTH_LONG).show()
-
                     }
-
                 }
-
             }
         }
 
@@ -79,6 +94,25 @@ class MainActivity : AppCompatActivity() {
         val ibGallery : ImageButton = findViewById(R.id.gallery)
         ibGallery.setOnClickListener{
             requestStoragePermission()
+        }
+
+        val ibUndo : ImageButton = findViewById(R.id.undo)
+        ibUndo.setOnClickListener{
+            drawingView?.onClickUndo()
+        }
+
+        val ibSave:ImageButton = findViewById(R.id.save)
+
+        ibSave.setOnClickListener{
+            if (isReadStorageAllowed()){
+                //launch a coroutine block
+                lifecycleScope.launch{
+                    //reference the frame layout
+                    val flDrawingView:FrameLayout = findViewById(R.id.drawing_view_container)
+                    //Save the image to the device
+                    saveBitmapFile(getBitmapFromView(flDrawingView))
+                }
+            }
         }
         //colorpicker
         /*val colorPicker : ImageButton = findViewById(R.id.btn_pick_color)
@@ -126,7 +160,6 @@ class MainActivity : AppCompatActivity() {
 //    }
 
 
-
     fun paintClicked(view: View){
         if(view !== mImageButtonCurrentPaint){
             val imageButton = view as ImageButton
@@ -143,8 +176,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun requestStoragePermission(){
 
+    private fun isReadStorageAllowed(): Boolean {
+        val result = ContextCompat.checkSelfPermission(
+            this, Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
+    //저장소 권한 체크
+    private fun requestStoragePermission(){
         if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE))
         {
            showRationaleDialog("Drawing App", "This Drawing App needs to Access your External Storage")
@@ -154,10 +195,7 @@ class MainActivity : AppCompatActivity() {
             ))
         }
     }
-
-
-
-    //권한체크 다이어로그
+    //권한체크 Dialog
     private fun showRationaleDialog(
         title: String,
         message: String,
@@ -170,6 +208,73 @@ class MainActivity : AppCompatActivity() {
                 dialog.dismiss()
             }
         builder.create().show()
+    }
+
+    private fun getBitmapFromView(view: View): Bitmap {
+
+        //Define a bitmap with the same size as the view.
+        // CreateBitmap : Returns a mutable bitmap with the specified width and height
+        val returnedBitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        //Bind a canvas to it
+        val canvas = Canvas(returnedBitmap)
+        //Get the view's background
+        val bgDrawable = view.background
+        if (bgDrawable != null) {
+            //has background drawable, then draw it on the canvas
+            bgDrawable.draw(canvas)
+        } else {
+            //does not have background drawable, then draw white background on the canvas
+            canvas.drawColor(Color.WHITE)
+        }
+        // draw the view on the canvas
+        view.draw(canvas)
+        //return the bitmap
+        return returnedBitmap
+    }
+
+    private suspend fun saveBitmapFile(mBitmap: Bitmap?):String{
+        var result = ""
+        withContext(Dispatchers.IO) {
+            if (mBitmap != null) {
+
+                try {
+                    val bytes = ByteArrayOutputStream() // Creates a new byte array output stream.
+                    // The buffer capacity is initially 32 bytes, though its size increases if necessary.
+
+                    mBitmap.compress(Bitmap.CompressFormat.PNG, 90, bytes)
+                    val f = File(
+                        externalCacheDir?.absoluteFile.toString()
+                                + File.separator + "KidDrawingApp_" + System.currentTimeMillis() / 1000 + ".jpg"
+                    )
+
+                    val fo =
+                        FileOutputStream(f) // Creates a file output stream to write to the file represented by the specified object.
+                    fo.write(bytes.toByteArray()) // Writes bytes from the specified byte array to this file output stream.
+                    fo.close() // Closes this file output stream and releases any system resources associated with this stream. This file output stream may no longer be used for writing bytes.
+                    result = f.absolutePath // The file absolute path is return as a result.
+                    //We switch from io to ui thread to show a toast
+                    runOnUiThread {
+                        if (!result.isEmpty()) {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "File saved successfully :$result",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Something went wrong while saving the file.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                } catch (e: Exception) {
+                    result = ""
+                    e.printStackTrace()
+                }
+            }
+        }
+        return result
     }
 
 }
